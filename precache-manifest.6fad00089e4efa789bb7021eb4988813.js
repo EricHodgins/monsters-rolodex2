f1819e7bb334bb6df800f260406b3e273d0747d7
EricHodgins <hodgins.e@gmail.com>
2020-10-17 14:07:18 -0400

    Updates

---
+++ precache-manifest.6fad00089e4efa789bb7021eb4988813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d723f2c2ddd74082b4c0972af5ccac",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "82911087b60d9ac55ed4",
+    "url": "/monsters-rolodex2/static/css/main.52fc3f81.chunk.css"
+  },
+  {
+    "revision": "4f4ac47c34e4c204eb15",
+    "url": "/monsters-rolodex2/static/js/2.e5d0314f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex2/static/js/2.e5d0314f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82911087b60d9ac55ed4",
+    "url": "/monsters-rolodex2/static/js/main.1a586784.chunk.js"
+  },
+  {
+    "revision": "5129b2734e446f0ec2c6",
+    "url": "/monsters-rolodex2/static/js/runtime-main.e8342c1e.js"
+  }
+]);
